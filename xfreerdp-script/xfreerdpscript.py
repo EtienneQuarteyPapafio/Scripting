976ded2ba11f526d85fc6169c190166e34c6bbba
Etienne Quartey-Papafio <68552839+EtienneQuarteyPapafio@users.noreply.github.com>
2026-08-09 15:00:46 +0100

    Add files via upload

--- xfreerdp-script/xfreerdpscript.py
+++ xfreerdp-script/xfreerdpscript.py
@@ -5,40 +5,69 @@
 print("This Python script will create a shell script that allows a user to connect to another \ncomputer using xfreerdp and set customisable options.")
 print("\nWould you like to proceed with this?")
 
-confirmation = int(input("1 for Yes, any other number for No\n"))
+confirmation = int(input("1 for Yes, or any other number for No\n"))
 
-print("Please enter your ip address in octects without the '.'\n")
+if confirmation == 1:
 
-octect1=int(input("Octect 1: "))
+    ip=0
 
-octect2=int(input("Octect 2: "))
+    while ip==0:
 
-octect3=int(input("Octect 3: "))
+        print("Please enter your ip address in octects without the '.'\n")
 
-octect4=int(input("Octect 4: "))
+        octect1=int(input("Octect 1: "))
 
-str(octect1)
-str(octect2)
-str(octect3)
-str(octect4)
+        octect2=int(input("Octect 2: "))
 
-ipAddress=str(octect1)+(".")+str(octect2)+(".")+str(octect3)+(".")+str(octect4)
+        octect3=int(input("Octect 3: "))
 
-print(ipAddress)
+        octect4=int(input("Octect 4: "))
 
-print("\nPlease enter target computers username\n")
+        str(octect1)
+        str(octect2)
+        str(octect3)
+        str(octect4)
 
-user=str(input("User: "))
+        ipAddress=str(octect1)+(".")+str(octect2)+(".")+str(octect3)+(".")+str(octect4)
 
-print("\nPlease enter target computers password\n")
+        print(ipAddress)
 
-password=str(input("Password: "))
-
-RDP=("xfreerdp")+(" ")+("/u:")+user+(" ")+("/p:")+password+(" ")+("/v:")+ipAddress
-
-f= open("RDPFile.sh","x")
-
-with open("RDPFile.sh","w") as f:
-    f.write(RDP)
+        ip=int(input("is this your ip address?\n1 for yes, any other number for no\n"))
 
 
+
+    u=0
+
+    while u==0:
+        print("\nPlease enter target computers username\n")
+
+        user=str(input("User: "))
+
+        print(user)
+
+        u=int(input("is this your user?\n1 for yes, any other number for no\n"))
+
+
+    p=0
+
+    while p==0:
+
+        print("\nPlease enter target computers password\n")
+
+        password=str(input("Password: "))
+
+        print(password)
+
+        p=int(input("is this your password?\n1 for yes, any other number for no\n"))        
+
+    RDP=("xfreerdp")+(" ")+("/u:")+user+(" ")+("/p:")+password+(" ")+("/v:")+ipAddress
+
+    f= open("RDPFile.sh","x")
+
+    with open("RDPFile.sh","w") as f:
+        f.write(RDP)
+
+
+else:
+    print("Exiting script")
+    quit()
